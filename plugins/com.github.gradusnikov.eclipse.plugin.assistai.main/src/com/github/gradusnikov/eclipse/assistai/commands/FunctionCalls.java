3b87a9b14e3639d6577efb425d452116edaeb680
wgrad <wgrad@IM-COMP-WG-004.ht.home>
2023-09-25 13:42:35 +0200

    Updating get javadoc function description

--- plugins/com.github.gradusnikov.eclipse.plugin.assistai.main/src/com/github/gradusnikov/eclipse/assistai/commands/FunctionCalls.java
+++ plugins/com.github.gradusnikov.eclipse.plugin.assistai.main/src/com/github/gradusnikov/eclipse/assistai/commands/FunctionCalls.java
@@ -34,9 +34,9 @@ public class FunctionCalls
     public record WeatherReport( String location, int degrees, String unit, String[] forecast ) {};
     
     
-    @Function(name="getJavaDoc", description="Get the JavaDoc for the given class.", type="object")
+    @Function(name="getJavaDoc", description="Get the JavaDoc for the given compilation unit.  For example,a class B defined as a member type of a class A in package x.y should have athe fully qualified name \"x.y.A.B\".Note that in order to be found, a type name (or its top level enclosingtype name) must match its corresponding compilation unit name.", type="object")
     public String getJavaDoc(
-            @FunctionParam(name="fullyQualifiedClassName", description="A fully qualified class name of the Java class", required=true) String fullyQualifiedClassName)
+            @FunctionParam(name="fullyQualifiedName", description="A fully qualified name of the compilation unit", required=true) String fullyQualifiedClassName)
     {
         return readJavaDocCommand.getClassAttachedJavadoc( fullyQualifiedClassName );
     }
