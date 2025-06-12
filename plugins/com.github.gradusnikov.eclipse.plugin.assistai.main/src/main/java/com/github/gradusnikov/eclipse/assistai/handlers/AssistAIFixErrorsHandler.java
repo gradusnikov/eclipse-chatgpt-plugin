0c83449d9e282b24a367e2835842548457028a9c
jsaintyv <jsaintyv@gmail.com>
2025-06-13 00:05:46 +0200

    Use Maven to manage dependencies and builds, prepare the codingagent library to isolate all code that does not need to be linked to the Eclipse codebase

---
+++ plugins/com.github.gradusnikov.eclipse.plugin.assistai.main/src/main/java/com/github/gradusnikov/eclipse/assistai/handlers/AssistAIFixErrorsHandler.java
@@ -0,0 +1,11 @@
+package com.github.gradusnikov.eclipse.assistai.handlers;
+
+import com.github.gradusnikov.eclipse.assistai.prompt.Prompts;
+
+public class AssistAIFixErrorsHandler extends AssistAIHandlerTemplate
+{
+    public AssistAIFixErrorsHandler()
+    { 
+        super( Prompts.FIX_ERRORS );
+    }
+}
